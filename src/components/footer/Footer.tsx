f026c53af4c48a9d90802f7e3ffbeeacd7c82b76
julia_ssaa <julia_ssaa@hotmail.com>
2024-01-17 13:55:20 +0100

    footer fix
    Co-authored-by: varamara <varamara@users.noreply.github.com>

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,29 +2,39 @@ import "./footer.scss";
 import PrimaryCircle from "../circles/PrimaryCircle";
 import SecondaryCircle from "../circles/SecondaryCircle";
 
-
 const Footer = () => {
   return (
     <section className="footer">
-      <div className="sales-txt">
-        <p><img src="src\assets\Check.svg" alt="check-mark" />Fraktfritt över 500:-</p>
-        <p><img src="src\assets\Check.svg" alt="check-mark" />3-5 dagars leveranstid</p>
-        <p><img src="src\assets\Check.svg" alt="check-mark" />Betala med Klarna</p>
+      <div className="footer-body">
+        <div className="sales-txt">
+          <p>
+            <img src="src\assets\Check.svg" alt="check-mark" />
+            Fraktfritt över 500:-
+          </p>
+          <p>
+            <img src="src\assets\Check.svg" alt="check-mark" />
+            3-5 dagars leveranstid
+          </p>
+          <p>
+            <img src="src\assets\Check.svg" alt="check-mark" />
+            Betala med Klarna
+          </p>
+        </div>
+        <PrimaryCircle />
+        <SecondaryCircle />
+        <div className="icons">
+          <a href="https://www.google.com/intl/sv/gmail/about/">
+            <img src="src\assets\at.png" />
+          </a>
+          <a href="https://www.facebook.com/">
+            <img src="src\assets\fb.png" />
+          </a>
+          <a href="https://www.instagram.com/">
+            <img src="src\assets\ig.png" />
+          </a>
+        </div>
+        <p>&copy; Konini 2024 </p>
       </div>
-      <PrimaryCircle />
-      <SecondaryCircle />
-      <div className="icons">
-        <a href="https://www.google.com/intl/sv/gmail/about/">
-          <img src="src\assets\at.png" />
-        </a>
-        <a href="https://www.facebook.com/">
-          <img src="src\assets\fb.png" />
-        </a>
-        <a href="https://www.instagram.com/">
-          <img src="src\assets\ig.png" />
-        </a>
-      </div>
-      <p>&copy; Konini 2024 </p>
     </section>
   );
 };
