72652abf039ee2644d382258f101ed9dd3f329af
varamara <mikahvaarama@gmail.com>
2024-01-18 10:01:14 +0100

    njk

--- src/components/productGrid/ProductGrid.tsx
+++ src/components/productGrid/ProductGrid.tsx
@@ -20,7 +20,7 @@ const ProductGrid = () => {
         {productService.products.length === 0 ? (
           <p>Loading...</p>
         ) : (
-          productService.products.slice(0, 12).map((product) => (
+          productService.products.slice(0, 10).map((product) => (
             <div key={product.id} className="product-card">
               <img src={product.image} alt={product.title} />
               <div className="product-info">
